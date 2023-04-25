57ba4e6b2fa6cf73de25022290e1612cf6ccebc7
Amarachi Dimkpa <amaradimkpa1@gmail.com>
2023-04-25 16:37:44 +0100

    Add event listener

--- src/components/MyCalculator.js
+++ src/components/MyCalculator.js
@@ -5,7 +5,7 @@ import Navigation from './Nav';
 
 const Calculator = () => {
   const [state, setState] = useState({ total: 0, next: null, operation: null });
-  const operateEvents = (e) => {
+  const createEvent = (e) => {
     const btnName = e.target.innerHTML;
     const result = calculate(state, btnName);
     setState(result);
@@ -20,61 +20,61 @@ const Calculator = () => {
           {operation}
           {next}
         </div>
-        <button type="button" className="ac btn" onClick={operateEvents}>
+        <button type="button" className="ac btn" onClick={createEvent}>
           AC
         </button>
-        <button type="button" className="one btn" onClick={operateEvents}>
+        <button type="button" className="one btn" onClick={createEvent}>
           +/-
         </button>
-        <button type="button" className="two btn" onClick={operateEvents}>
+        <button type="button" className="two btn" onClick={createEvent}>
           %
         </button>
-        <button type="button" className="tree btn" onClick={operateEvents}>
+        <button type="button" className="tree btn" onClick={createEvent}>
           ÷
         </button>
-        <button type="button" className="four btn" onClick={operateEvents}>
+        <button type="button" className="four btn" onClick={createEvent}>
           7
         </button>
-        <button type="button" className="five btn" onClick={operateEvents}>
+        <button type="button" className="five btn" onClick={createEvent}>
           8
         </button>
-        <button type="button" className="six btn" onClick={operateEvents}>
+        <button type="button" className="six btn" onClick={createEvent}>
           9
         </button>
-        <button type="button" className="seven btn" onClick={operateEvents}>
+        <button type="button" className="seven btn" onClick={createEvent}>
           x
         </button>
-        <button type="button" className="eigth btn" onClick={operateEvents}>
+        <button type="button" className="eigth btn" onClick={createEvent}>
           4
         </button>
-        <button type="button" className="nine btn" onClick={operateEvents}>
+        <button type="button" className="nine btn" onClick={createEvent}>
           5
         </button>
-        <button type="button" className="ten btn" onClick={operateEvents}>
+        <button type="button" className="ten btn" onClick={createEvent}>
           6
         </button>
-        <button type="button" className="eleven btn" onClick={operateEvents}>
+        <button type="button" className="eleven btn" onClick={createEvent}>
           -
         </button>
-        <button type="button" className="twelve btn" onClick={operateEvents}>
+        <button type="button" className="twelve btn" onClick={createEvent}>
           1
         </button>
-        <button type="button" className="thirteen btn" onClick={operateEvents}>
+        <button type="button" className="thirteen btn" onClick={createEvent}>
           2
         </button>
-        <button type="button" className="fourteen btn" onClick={operateEvents}>
+        <button type="button" className="fourteen btn" onClick={createEvent}>
           3
         </button>
-        <button type="button" className="fifteen btn" onClick={operateEvents}>
+        <button type="button" className="fifteen btn" onClick={createEvent}>
           +
         </button>
-        <button type="button" className="seventeen btn" onClick={operateEvents}>
+        <button type="button" className="seventeen btn" onClick={createEvent}>
           0
         </button>
-        <button type="button" className="eighteen btn" onClick={operateEvents}>
+        <button type="button" className="eighteen btn" onClick={createEvent}>
           .
         </button>
-        <button type="button" className="nineteen btn" onClick={operateEvents}>
+        <button type="button" className="nineteen btn" onClick={createEvent}>
           =
         </button>
       </div>
